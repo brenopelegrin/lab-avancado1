8e4a25a44fffcb2e4314e93849639c2a98dfde96
viniciusdutra314 <dutravinisousa2@usp.br>
2025-09-18 01:20:54 +0000

    fix: insane float error bug  Júpiter

--- velocidade_da_luz/pulsos/pulsos.py
+++ velocidade_da_luz/pulsos/pulsos.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import numpy as np 
 
-distancias=2*lab.arrayM([472,743,1003,1276],"cm",5)
-tempos=lab.arrayM([25,50,72,88],"ns",1)
+distancias=2*lab.arrayM([0,472,743,1003,1276],"cm",5)
+tempos=lab.arrayM([0,25,50,72,88],"ns",1)
 
 unidade_distancia="m"
 unidade_tempo="ns"
@@ -18,8 +18,8 @@ a,pcov=curve_fit(lambda x,a: a*x,
 c=Medida(a[0],f"{unidade_distancia}/{unidade_tempo}",np.sqrt(pcov[0][0]))
 x_values=lab.linspaceM(min(lab.nominais(tempos,unidade_tempo)),
                      max(lab.nominais(tempos,unidade_tempo)),100,unidade_tempo,0)
-axis.set_xlabel(f"Tempo ({unidade_tempo})")
-axis.set_ylabel(f"Distância ({unidade_distancia})")
+axis.set_xlabel(f"Tempo de voo ({unidade_tempo})")
+axis.set_ylabel(f"Distância ida e volta ({unidade_distancia})")
 axis.set_title(f"Medição por pulsos c={c:km/s}")
 axis.scatter(lab.nominais(tempos,unidade_tempo),
              lab.nominais(distancias,unidade_distancia),
