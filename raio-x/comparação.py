c43403a0d65ae0f75c5cd352fed058372cc144ac
viniciusdutra314 <dutravinisousa2@usp.br>
2025-11-30 12:02:23 -0300

    WIP: save state

--- raio-x/comparação.py
+++ raio-x/comparação.py
@@ -5,6 +5,22 @@ import os
 import json 
 import pandas as pd 
 
+def interpolate_log_log_antonio(E_target, E1, mu1, E2, mu2):
+    """
+    Executa a interpolação log-log para encontrar mu_target.
+    """
+    log_E_target = np.log(E_target)
+    log_E1 = np.log(E1)
+    log_E2 = np.log(E2)
+    log_mu1 = np.log(mu1)
+    log_mu2 = np.log(mu2)
+
+    log_mu_target = log_mu1 + (log_mu2 - log_mu1) * (log_E_target - log_E1) / (log_E2 - log_E1)
+
+    return np.exp(log_mu_target)
+
+
+
 plt.style.use("ggplot")
 fig,ax=plt.subplots(figsize=(5,5),dpi=150)
 ax.grid(True)
@@ -31,9 +47,15 @@ for Z in Z_values:
     energies = np.array(energies)
     mu_over_rho = np.array(mu_values)
     idx = (np.abs(energies - E_k_alpha)).argmin()
-    mu_over_rho_values.append(mu_over_rho[idx])
+    mu=interpolate_log_log_antonio(E_k_alpha, 
+                                   energies[idx-1], 
+                                   mu_over_rho[idx-1], 
+                                   energies[idx], 
+                                   mu_over_rho[idx])
+    mu_over_rho_values.append(mu)
 
 ax.scatter(Z_values, mu_over_rho_values, label='NIST', color='black',s=10)
 ax.plot(Z_values, mu_over_rho_values, color='black',linewidth=0.5,linestyle='dashed')
 ax.legend()
+
 fig.savefig("comparacao.png")
